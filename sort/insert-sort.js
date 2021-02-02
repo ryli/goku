9b2da3c83618575d75ea428b4300a3fdd6d15f74
yaf <lov.2014@gmail.com>
2021-02-02 19:46:47 +0800

    fix: insert

--- sort/insert-sort.js
+++ sort/insert-sort.js
@@ -3,8 +3,8 @@ export default function insertSort(originalArray) {
   const array = [...originalArray]
   for (let i = 0; i < array.length; i += 1) {
     // 退出条件
-    for (let j = i; j > 0 && array[j - 1] > array[i]; j -= 1) {
-      [array[i - 1], array[i]] = [array[i], array[i - 1]]
+    for (let j = i; j > 0 && array[j - 1] > array[j]; j -= 1) {
+      [array[j - 1], array[j]] = [array[j], array[j - 1]]
     }
   }
   return array
